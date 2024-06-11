ee07e2fa7a61b79c8478650e861bb68e7f074ef5
beaup <tom.beaupuis@efrei.net>
2024-06-11 11:15:49 +0200

    debug

--- Projet/star_wars_project/Assets/Scripts/Characters/CharacterInteraction.cs
+++ Projet/star_wars_project/Assets/Scripts/Characters/CharacterInteraction.cs
@@ -24,28 +24,3 @@ public class CharacterInteraction : MonoBehaviour
     }
 }
 
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-
-public class CharacterInteraction : MonoBehaviour
-{
-    private void OnCollisionExit(Collision collision)
-    {
-        ExitCharacterCollisionArea(collision);
-    }
-
-    private void ExitCharacterCollisionArea(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("PNJ"))
-        {
-            
-            GameObject[] canvases = GameObject.FindGameObjectsWithTag("CanvasPNJ");
-            foreach (GameObject canvas in canvases)
-            {
-                Destroy(canvas);
-            }
-        }
-    }
-}
